23a64e55e0001091d21a35763f2c38970ed750ed
iara-elise <iara.elise@escolar.ifrn.edu.br>
2024-04-03 11:13:15 -0300

    inicializando repositorio

---
+++ Aula_01/Programa-01.py
@@ -0,0 +1,25 @@
+print('Insira dois valore inteiros abaixo:')
+valor_01 = int(input('Valor 01: '))
+valor_02 = int(input('Valor 02: '))
+
+print('SOMA:')
+soma = valor_01 + valor_02
+print('RESULTADO: ', soma)
+
+print()
+
+print('MULTIPLICAÇÃO')
+multiplicacao = valor_01 * valor_02
+print('RESULTADO: ', multiplicacao)
+
+print()
+
+print('SUBTRAÇÃO')
+subtracao = valor_01 - valor_02
+print('RESULTADO: ', subtracao)
+
+print()
+
+print('DIVISÃO')
+divisao = valor_01 / valor_02
+print('RESULTADO: ', divisao)
