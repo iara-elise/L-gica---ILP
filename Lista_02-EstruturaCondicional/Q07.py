6d3bded801d7a7e7472edc60200a48b8382c4631
Iara Elise <iara.elise@escolar.ifrn.edu.br>
2024-07-29 16:17:38 -0300

    Iniciando questão 7.

---
+++ Lista_02-EstruturaCondicional/Q07.py
@@ -0,0 +1,16 @@
+valor_01 = int(input("Valor 01:"))
+valor_02 = int(input("Valor 02:"))
+valor_03 = int(input("Valor 03:"))
+
+c_ou_d = input("Deseja ver os valores em ordem crescente ou decrescente?")
+
+if c_ou_d == 'Crescente':
+
+    if valor_01 < valor_02 and valor_01 < valor_03 and valor_02 < valor_03:
+     print(f"{valor_01}, {valor_02}, {valor_03}")
+
+    elif valor_02 < valor_01 and valor_02 < valor_03 and valor_03 < valor_01:
+     print(f"{valor_02}, {valor_03}, {valor_01}")
+
+    else:
+     print(f"{valor_03}, {valor_02}, {valor_01}")
