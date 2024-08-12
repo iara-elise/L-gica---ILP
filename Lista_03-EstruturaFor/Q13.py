eb24476e378704f4ac802fb3da2ad7d4e76d5ebc
Iara Elise <iara.elise@escolar.ifrn.edu.br>
2024-08-12 14:56:03 -0300

    Ajustes Q13L3

--- Lista_03-EstruturaFor/Q13.py
+++ Lista_03-EstruturaFor/Q13.py
@@ -2,7 +2,7 @@ tabuada = 0
 
 num = int(input("Digite um número entre 1 e 10: "))
 
-for i in range(9):
+for i in range(10):
     tabuada = (i + 1) * num
     
     print(num, " * ", i + 1, "=", tabuada)
