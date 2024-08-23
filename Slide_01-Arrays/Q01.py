719b2c1241c62994a773dc3c4ee502528b9629db
Iara Elise <iara.elise@escolar.ifrn.edu.br>
2024-08-22 23:27:21 -0300

    Comentando questões 01 e 02.

--- Slide_01-Arrays/Q01.py
+++ Slide_01-Arrays/Q01.py
@@ -1,13 +1,26 @@
+# PARTE 1
+# Declarando listas vazias.  
+
 listaNum = []
 novaLista = []
 
+# PARTE 2
+# A variável "qtdNum" armazena a quantidade de itens que o usuário deseja guardar na lista.
 
 qtdNum = int(input("Quantos números você deseja inserir? \n"))
+
+# PARTE 3
+# O for percorre a quantidade de números inseridos pelo usuário para declarar os itens da lista através da variável "num". A entrada é dada pelo usuário. Após a entrada de dados, a função "append" adiciona "num" a "listaNum".
 
 for i in range(qtdNum):
     num = int(input())
 
     listaNum.append(num)
+
+# PARTE 4
+# O for utiliza a variável "num" para percorrer a "listaNum".
+# Dentro do for, há uma condicional com a seguinte condição: se "num" não existir em "novaLista", "num" será adicionado à "novaLista", ou seja, ela verifica se há números repetidos e se houver, eles não serão adicionados à "novaLista".
+# Ao final do programa, a "novaLista" é impressa sem números repetidos.
 
 for num in listaNum:
     if num not in novaLista:
--- Slide_01-Arrays/Q04.py
+++ Slide_01-Arrays/Q04.py
@@ -1,11 +1,23 @@
+# PARTE 1
+# Declarando lista vazia.  
+
 listaNum = []
 
+# PARTE 2
+# A variável "qtdNum" armazena a quantidade de itens que o usuário deseja guardar na lista.
+
 qtdNum= int(input("Quantos números você deseja inserir? \n"))
+
+# PARTE 3
+# O for percorre a quantidade de números inseridos pelo usuário para declarar os itens da lista através da variável "num". A entrada é dada pelo usuário. Após a entrada de dados, a função "append" adiciona "num" a "listaNum".
 
 for i in range(qtdNum):
     num = int(input())
 
     listaNum.append(num)
+
+# PARTE 4
+# Utilizando list comprehension, a variável "i" é elevada ao quadrado, em seguida o laço for percorre "listaNum", uma condicional verifica se o valor armazenado em "i" é impar. Por fim, "novaLista" é impressa.
 
 novaLista = [i ** 2 for i in listaNum if i % 2 != 0]
 
